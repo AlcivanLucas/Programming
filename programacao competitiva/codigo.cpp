126f9fa6b36e7492a29b3e8af657b5f0368074e1
AlcivanLucas <alcivan14lucas@gmail.com>
2024-12-15 21:35:25 -0300

    FEAT: prog competitiva

---
+++ programacao competitiva/codigo.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int idade = 30;
+    string nome = "João";
+    cout << "Meu nome é " << nome << " e tenho " << idade << " anos." << endl;
+    return 0;
+}
